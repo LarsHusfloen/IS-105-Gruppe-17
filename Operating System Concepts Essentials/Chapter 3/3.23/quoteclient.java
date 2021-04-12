3d7a4db41f0cd44f43ad6a2b53c0c4cdca3a87be
Lars <larsbh@uia.no>
2021-04-12 11:33:20 +0200

    Finished 3.23

--- Operating System Concepts Essentials/Chapter 3/3.23/quoteclient.java
+++ Operating System Concepts Essentials/Chapter 3/3.23/quoteclient.java
@@ -4,18 +4,16 @@ import java.io.*;
 public class quoteclient {
     public static void main(String[] args) {
         try {
-            /* make connection to server socket */
-            Socket sock = new Socket("127.0.0.1", 6017);
-            InputStream in = sock.getInputStream();
-            BufferedReader bin = new BufferedReader(new InputStreamReader(in));
-            /* read the date from the socket */
+            Socket sock = new Socket("127.0.0.1", 6017); /* make connection to server socket */
+            InputStream in = sock.getInputStream(); /* Where to get the input stream from socket */
+            BufferedReader bin = new BufferedReader(new InputStreamReader(in)); /* Read the quote from the socket */
+            
             String line;
-            while ((line = bin.readLine()) != null)
-                System.out.println(line);
-            /* close the socket connection */
-            sock.close();
+            while ((line = bin.readLine()) != null) /* True if socket contain anything */
+                System.out.println(line); /* Prints in the terminal */
+            sock.close(); /* close the socket connection */
         } catch (IOException ioe) {
-            System.err.println(ioe);
+            System.err.println(ioe); /* Prints exception if it couldnt establish connection to socket and read/write */
         }
     }
 }
--- Operating System Concepts Essentials/Chapter 3/3.23/quoteserver.java
+++ Operating System Concepts Essentials/Chapter 3/3.23/quoteserver.java
@@ -1,29 +1,33 @@
 import java.net.*;
 import java.io.*;
+import java.util.Random;
+import java.util.ArrayList;
 
-public class quoteserver
-{
-public
-    static void main(String[] args)
-    {
-        try
-        {
-            ServerSocket sock = new ServerSocket(6017);
-            /* now listen for connections */
-            while (true)
-            {
-                Socket client = sock.accept();
-                PrintWriter pout = new PrintWriter(client.getOutputStream(), true);
-                /* write the Date to the socket */
-                pout.println(new java.util.Date().toString());
-                /* close the socket and resume */
-                /* listening for connections */
-                client.close();
+public class quoteserver {
+    private static ArrayList<String> quotes = new ArrayList<>(); /* Creates an arraylist for String called quotes */
+    private static Random rand = new Random(); /* Creates a Random object called rand */
+
+    public static void main(String[] args) {
+        quote(); /* Runs the method quote() */
+        try {
+            ServerSocket sock = new ServerSocket(6017); /* Listens for connections */
+            while (true) { /* If connections equals true, run the following code */
+                Socket client = sock.accept(); /* Establish the connection to the socket */
+                PrintWriter pout = new PrintWriter(client.getOutputStream(), true); /* How to write to the socket */
+                pout.println("Quote of the day:"); /* Write the string to the socket */
+                pout.println(quotes.get(rand.nextInt(quotes.size()))); /* write the quote to the socket */
+                client.close(); /* close the socket and resume */
             }
+        } catch (IOException ioe) {
+            System.err.println(ioe); /* Prints exception if it couldnt establish connection to socket and read/write */
         }
-        catch (IOException ioe)
-        {
-            System.err.println(ioe);
-        }
+    }
+
+    private static void quote() { /* Add the following quotes to the arraylist */
+        quotes.add("Be yourself; everyone else is already taken.");
+        quotes.add("Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.");
+        quotes.add("You only live once, but if you do it right, once is enough.");
+        quotes.add("Be the change that you wish to see in the world.");
+        quotes.add("In three words I can sum up everything I've learned about life: it goes on.");
     }
 }
