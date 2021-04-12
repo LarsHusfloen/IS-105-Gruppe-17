c1c3d71f25bb2df74634ec8f1661c05d9fc9cd55
Lars <larsbh@uia.no>
2021-04-12 19:44:07 +0200

    Finished 3.25

--- Operating System Concepts Essentials/Chapter 3/3.25/echoserver.java
+++ Operating System Concepts Essentials/Chapter 3/3.25/echoserver.java
@@ -4,27 +4,20 @@ import java.io.*;
 public class echoserver {
 
     public static void main(String[] args) {
-
         try {
             ServerSocket sock = new ServerSocket(6017); /* Listens for connections */
-            while (true) { /* If connections equals true, run the following code */
-                Socket client = sock.accept(); /* Establish the connection to the socket */
-                PrintWriter pout = new PrintWriter(client.getOutputStream(), true); /* How to write to the socket */
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream())); /* Read the quote from the socket */
+            Socket client = sock.accept(); /* Establish the connection to the socket */
+            PrintWriter pout = new PrintWriter(client.getOutputStream(), true); /* Output location */
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream())); /* Read the input from the socket */
 
-                
-                String line;
-                while ((line = in.readLine()) != null){ /* True if socket contain anything */
-                    pout.println("Server echo back: " + line); /* Prints in the terminal */
-                }
-                client.close(); /* close the socket and resume */
-                //in.close();
-                //pout.close();
+            String line;
+            while ((line = in.readLine()) != null) { /* True if socket contain anything */
+                pout.println("Server echo back: " + line); /* Puts it in the socket */
+                break; /* Closes the client and server */
             }
+            client.close(); /* close the socket */
         } catch (IOException ioe) {
-            System.err.println(ioe); /*
-                                      * Prints exception if it couldnt establish connection to socket and read/write
-                                      */
+            System.err.println(ioe); /* Prints exception if it couldnt establish connection to socket and read/write */
         }
     }
 }
