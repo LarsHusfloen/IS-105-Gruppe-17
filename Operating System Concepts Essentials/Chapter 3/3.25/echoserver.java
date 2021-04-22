450bd45bd08122365579e0e6580560da2b0d83b8
LarsHusfloen <79321698+LarsHusfloen@users.noreply.github.com>
2021-04-22 11:35:46 +0200

    Update echoserver.java

--- Operating System Concepts Essentials/Chapter 3/3.25/echoserver.java
+++ Operating System Concepts Essentials/Chapter 3/3.25/echoserver.java
@@ -6,19 +6,19 @@ public class echoserver {
     public static void main(String[] args) {
         try {
             ServerSocket sock = new ServerSocket(6017); /* Listens for connections */
-            Socket client = sock.accept(); /* Establish the connection to the socket */
-            PrintWriter pout = new PrintWriter(client.getOutputStream(), true); /* Output location */
+            Socket client = sock.accept();              /* Establish the connection to the socket */
+            PrintWriter pout = new PrintWriter(client.getOutputStream(), true);                     /* Output location */
             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream())); /* Read the input from the socket */
 
             do{
                 pout.println("Server echo back: " + in.readLine()); /* Puts it in the socket */
-                break; /* Closes the client and server */
+                break;                  /* Closes the client and server */
             }
-            while (in.read() != -1);  /* True if socket contain anything */
+            while (in.read() != -1);    /* True if socket contain anything */
             
-            client.close(); /* close the socket */
+            client.close();             /* close the socket */
         } catch (IOException ioe) {
-            System.err.println(ioe); /* Prints exception if it couldnt establish connection to socket and read/write */
+            System.err.println(ioe);    /* Prints exception if it couldnt establish connection to socket and read/write */
         }
     }
 }
