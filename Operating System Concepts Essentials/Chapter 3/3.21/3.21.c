0c86744a0bf62df2cb8745b2dc6b523d6350436d
LarsHusfloen <79321698+LarsHusfloen@users.noreply.github.com>
2021-04-22 11:27:58 +0200

    Update 3.21.c

--- Operating System Concepts Essentials/Chapter 3/3.21/3.21.c
+++ Operating System Concepts Essentials/Chapter 3/3.21/3.21.c
@@ -5,28 +5,28 @@
 
 int main()
 {
-    pid_t pid; /* defines pid as the name of variable of the type pid_t */
-    int num;   /* defines num as the name of variable of the type integer */
+    pid_t pid; 
+    int num;   
 
-    printf("Enter the number to run: "); /* prints the string to the terminal */
-    scanf("%d", &num);                   /* takes the input and setts the value to num */
+    printf("Enter the number to run: "); 
+    scanf("%d", &num);                   
     if (num <= 0)
     {                                                      /* Checks if the input is a positive integer */
-        printf("The number must be a positiv integer.\n"); /* Prints the string in terminal */
-        exit(0);                                           /* Exits the program */
+        printf("The number must be a positiv integer.\n"); /
+        exit(0);                                           
     }
 
-    pid = fork(); /* makes a child process and stores it's id to pid */
+    pid = fork(); 
 
     if (pid < 0) /* checks if the child process was establihed properly */
     {
-        printf("Failed to create fork.\n"); /* prints the string to the terminal */
+        printf("Failed to create fork.\n"); 
         return -1;
     }
 
     if (pid == 0) /* checks if the pid is equal to 0, if true it's the child process */
     {
-        printf("Start of child process\n"); /* prints the string to the terminal */
+        printf("Start of child process\n"); 
         while (num > 1)                     /* Ensures that the following code will run as long as num is higher than 1, therefor it stops when num reaches 1 */
         {
             printf("%d\n", num); /* Prints the value of num for every loop */
@@ -40,13 +40,13 @@ int main()
             }
         }
         printf("%d\n", num);                   /* Prints the value of num */
-        printf("The child process is done\n"); /* prints the string to the terminal */
+        printf("The child process is done\n"); 
     }
     else
     {
-        printf("Parent process wait for child process.\n"); /* prints the string to the terminal */
+        printf("Parent process wait for child process.\n"); 
         wait(NULL);                                         /* Parent process wait for child process to complete */
-        printf("The parent process is done\n");             /* prints the string to the terminal */
+        printf("The parent process is done\n");             
     }
     return 0;
 }
