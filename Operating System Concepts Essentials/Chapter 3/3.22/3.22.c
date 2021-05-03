c490de1ff3c8ed53380747b57bab1e822be7ebbf
LarsHusfloen <79321698+LarsHusfloen@users.noreply.github.com>
2021-05-03 14:50:15 +0200

    Update 3.22.c

--- Operating System Concepts Essentials/Chapter 3/3.22/3.22.c
+++ Operating System Concepts Essentials/Chapter 3/3.22/3.22.c
@@ -1,3 +1,6 @@
+/* Deler av kode hentet eller modifisert fra 
+Silverschatz figure 3.17 og 3.18 side 132 & 133 */
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -13,65 +16,88 @@ int main()
     pid_t pid;                    
     int num, shm_fd, size = 4096; 
     char *name = "SharedMemory";  
-    void *ptr;                    /* Setts the pointer to be used to transfer the data to and from the shared memory */
-
-    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+    
+    /* Setts the pointer to be used to transfer the data to and from the shared memory */
+    void *ptr;
+    
     /* shm_open creates a shared memory object, parameters: name of shared memory, O_CREAT; create if it doesnt exist,
      O_RDWR; open to read and write, gives the directory permisson for the object */
+    shm_fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+    
     if (shm_fd < 0)
-    {                                             /* Checks if the memory object was created succsesfully */
+    {                                             
         printf("Couldn't create chared memory."); 
         return -1;
     }
-    ftruncate(shm_fd, size);                                /* Configures the size of the object in bytes */
-    ptr = mmap(0, size, PROT_WRITE, MAP_SHARED, shm_fd, 0); /* Establishes a memory mapped file containing the shared memory object,
+    
+    /* Configures the size of the object in bytes */
+    ftruncate(shm_fd, size);
+    
+    /* Establishes a memory mapped file containing the shared memory object,
      also returns a pointer to the file that is used to access the memory object */
+    ptr = mmap(0, size, PROT_WRITE, MAP_SHARED, shm_fd, 0); 
 
     printf("Enter a positiv number to run through The Collatz conjecture: "); 
     scanf("%d", &num);                                                        
     if (num <= 0)
-    {                                                      /* Checks if the input is a positive integer */
+    {                                                      
         printf("The number must be a positiv integer.\n"); 
         exit(0);                                           
     }
 
     pid = fork(); 
-
-    if (pid < 0) /* checks if the child process was establihed properly */
+    
+    /* checks if the child process was establihed properly */
+    if (pid < 0) 
     {
         printf("Failed to create fork.\n"); 
         return -1;
     }
-
-    if (pid == 0) /* checks if the pid is equal to 0, if true it's the child process */
+    
+    /* checks the pid for child process */
+    if (pid == 0) 
     {
         printf("Start of child process\n"); 
 
-        while (num > 1) /* Ensures that the following code will run as long as num is higher than 1, therefor it stops when num reaches 1 */
+        /* Calculation according to The Collatz conjecture while above 1 */
+        while (num > 1)
         {
-            sprintf(ptr, "%d\n", num);   /* Writes to the shared memory object */
-            int length = log10(num) + 2; /* Calculates the length of num, e.g. 22 = 2, 123 = 3. Includes space for \n */
-            ptr += length;               /* Increments the pointer */
-            if (num % 2 == 0)            /* Checks if num is even or odd, if true num is even */
+            /* Writes to the shared memory object */
+            sprintf(ptr, "%d\n", num);   
+            
+            /* Calculates the length of num, e.g. 22 = 2, 123 = 3. Includes space for \n */
+            int length = log10(num) + 2; 
+            
+            /* Increments the pointer */
+            ptr += length;               
+            
+            /* Checks if num is even or odd */
+            if (num % 2 == 0)            
             {
-                num = num / 2; /* Calculation according to The Collatz conjecture */
+                num = num / 2; 
             }
-            else if (num % 2 == 1) /* Checks if num is even or odd, if true num is odd */
+            else if (num % 2 == 1) 
             {
-                num = 3 * num + 1; /* Calculation according to The Collatz conjecture */
+                num = 3 * num + 1; 
             }
         }
-        sprintf(ptr, "%d\n", num);             /* Writes to the shared memory object */
+        /* Writes to the shared memory object */
+        sprintf(ptr, "%d\n", num);             
         printf("The child process is done\n"); 
     }
     else
     {
+        /* Parent process wait for child process to complete */
         printf("Parent process wait for child process.\n"); 
-        wait(NULL);                                         /* Parent process wait for child process to complete */
-        printf("Parent:\n%s", (char *)ptr);                 /* Read/print from the shared memory object */
+        wait(NULL);                                         
+        
+        /* Read/print from the shared memory object */
+        printf("Parent:\n%s", (char *)ptr);                 
 
         printf("The parent process is done\n"); 
-        shm_unlink(name);                       /* Removes the shared memory object */
+        
+        /* Removes the shared memory object */
+        shm_unlink(name);                       
     }
     return 0;
 }
