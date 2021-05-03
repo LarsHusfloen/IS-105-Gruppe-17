d3ead383750b20e6fead2423a765a26179f22eaa
LarsHusfloen <79321698+LarsHusfloen@users.noreply.github.com>
2021-05-03 22:03:20 +0200

    Update pipe.c

--- Operating System Concepts Essentials/Chapter 3/3.26/pipe.c
+++ Operating System Concepts Essentials/Chapter 3/3.26/pipe.c
@@ -1,3 +1,6 @@
+/* Deler av kode hentet eller modifisert fra 
+Silverschatz figure 3.25 & 3.26 side 143 & 144 */
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -16,52 +19,71 @@ int main()
 	pid_t pid;								 
 	printf("Enter the string to reverse: "); 
 	fgets(write_msg, size, stdin);			
-	write_msg[strcspn(write_msg, "\n")] = 0; 	/* Cleares the input buffer */
+	
+	/* Cleares the input buffer */
+	write_msg[strcspn(write_msg, "\n")] = 0; 	
 
-	if (pipe(fd) == -1) 	/* create the pipe and chechs if it was created correctly*/
+	/* create the pipe and chechs if it was created correctly*/
+	if (pipe(fd) == -1) 	
 	{
 		printf("Failed tp create pipe.\n"); 
 		return 1;
 	}
 
 	pid = fork(); 
-	if (pid < 0)  /* checks if the child process was establihed properly */
+	if (pid < 0)
 	{
 		printf("Failed to create fork.\n"); 
 		return -1;
 	}
-
-	if (pid == 0) /* checks if the pid is equal to 0, if true it's the child process */
+	
+	/* checks the pid for child process */
+	if (pid == 0) 
 	{
 		printf("Start of child process\n");	   
-		read(fd[READ_END], read_msg, size);	/* reads from pipe and assignes it to read_msg */
-		close(fd[READ_END]);			/* closes one side of the pipe*/
-		for (i = 0; i < strlen(read_msg); i++) 	/* convert from upper to lowercase and vice versa */
+		
+		/* reads from pipe and assignes it to read_msg */
+		read(fd[READ_END], read_msg, size);	
+		
+		/* closes one side of the pipe*/
+		close(fd[READ_END]);			
+		
+		/* convert from upper to lowercase and vice versa */
+		for (i = 0; i < strlen(read_msg); i++) 	
 		{
-			if (islower(read_msg[i])) 	/* Check if it is lowercase */
+			if (islower(read_msg[i])) 	
 			{
-				write_msg[i] = toupper(read_msg[i]); /* Convert to uppercase */
+				write_msg[i] = toupper(read_msg[i]); 
 			}
 			else
 			{
-				write_msg[i] = tolower(read_msg[i]); /* Convert to lowercase */
+				write_msg[i] = tolower(read_msg[i]); 
 			}
 		}
-		write(fd[WRITE_END], write_msg, size); 		/* Writes the new msg to the pipe */
-		close(fd[WRITE_END]);				/* Closes the final side of the pipe */
+		
+		/* Writes the new msg to the pipe and closes the final side of the pipe*/
+		write(fd[WRITE_END], write_msg, size); 		
+		close(fd[WRITE_END]);				
 		printf("The child process is done\n"); 
 	}
 	else
 	{
-		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1); 	/* Writes the input from terminal into the pipe */
+		/* Writes the input from terminal into the pipe */
+		write(fd[WRITE_END], write_msg, strlen(write_msg) + 1); 	
 		printf("Written to the pipe: %s\n", write_msg);			
-		close(fd[WRITE_END]);						/* closes one side of the pipe*/
+		
+		/* closes one side of the pipe*/
+		close(fd[WRITE_END]);						
 
 		printf("Parent process wait for child process.\n"); 
-		wait(NULL);							/* Parent process wait for child process to complete */
-		read(fd[READ_END], read_msg, size);				/* reads from pipe and assignes it to read_msg */
+		wait(NULL);							
+		
+		/* reads from pipe and assignes it to read_msg */
+		read(fd[READ_END], read_msg, size);				
 		printf("Reading from the pipe: %s\n", read_msg);	
-		close(fd[READ_END]);						/* Closes the final side of the pipe */
+		
+		/* Closes the final side of the pipe */
+		close(fd[READ_END]);						
 		printf("The parent process is done\n");				
 	}
 	return 0;
