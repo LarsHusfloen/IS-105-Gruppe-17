5c31b3b52b882b18c1cbcdbc2f4fec2d3d9ade9f
LarsHusfloen <79321698+LarsHusfloen@users.noreply.github.com>
2021-05-03 13:30:06 +0200

    Update 3.19.c

--- Operating System Concepts Essentials/Chapter 3/3.19/3.19.c
+++ Operating System Concepts Essentials/Chapter 3/3.19/3.19.c
@@ -8,20 +8,22 @@ int main()
 
     pid = fork(); 
 
-    if (pid < 0) /* checks if the child process was establihed properly */
+    /* checks if the child process was establihed properly */
+    if (pid < 0) 
     {
         printf("Failed to create fork.\n"); 
         return -1;
     }
 
-    if (pid > 0) /* checks if the pid is bigger than 0, if true it's the parent process */
+    /* checks if the pid corresponds with parent or child process */
+    if (pid > 0) 
     {
-        printf("Parent process with pid: %d\n", getpid()); /* prints the string to the terminal, with the parents pid */
+        printf("Parent process with pid: %d\n", getpid());
         sleep(10);                                         
     }
-    else if (pid == 0) /* checks if the pid is equal to 0, if true it's the child process */
+    else if (pid == 0) 
     {
-        printf("Child process with pid: %d\n", getpid()); /* prints the string to the terminal, with the child pid */
+        printf("Child process with pid: %d\n", getpid());
         exit(0);                                          
     }
     return 0;
