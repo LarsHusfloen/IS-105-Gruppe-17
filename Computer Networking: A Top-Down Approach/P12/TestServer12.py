a7c18897c632daeda0ef45274d7666e78240292d
Lars <larsbh@uia.no>
2021-05-04 17:16:36 +0200

    .decode()

--- Computer Networking: A Top-Down Approach/P12/TestServer12.py
+++ Computer Networking: A Top-Down Approach/P12/TestServer12.py
@@ -1,5 +1,5 @@
 from socket import *
-serverPort=12000
+serverPort=80
 serverSocket=socket(AF_INET,SOCK_STREAM)
 serverSocket.bind(("",serverPort))
 serverSocket.listen(1)
@@ -7,5 +7,5 @@ serverSocket.listen(1)
 while 1:
 	connectionSocket, addr = serverSocket.accept()
 	Info = connectionSocket.recv(1024)
-	print('The server said:', Info, '\n')
+	print('The server said:', Info.decode(), '\n')
 serverSocket.close()
